6a848ce325f4d4c346b7bda96b48ba48214e871f
Harikrishnan <harikrishnan8421@gmail.com>
2020-07-24 19:08:49 +0530

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -16,8 +16,9 @@ public class Finance {
     {
         String command=args[0];
         boolean isValidCommand=validateCommandArguments(args);
-        if(!commandsToUsage.containsKey(command)){
+        if(!commandsToUsage.containsKey(command)) {
             System.out.println(command + ": command not found");
+            return;
         }
         if(!isValidCommand){
             System.out.println(commandsToUsage.get(args[0]));
